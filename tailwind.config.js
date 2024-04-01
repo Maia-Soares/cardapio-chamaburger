2db564f250946d95b78773899545453a12bc7b66
Maiana Soares <maiana.a.soares@gmail.com>
2024-04-01 10:44:26 -0300

    cardapio online para a hamburgueria chama burger

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./**/*.{html,js}'],
+  theme: {
+    extend: {
+      backgroundImage: {
+        home: "url('/assets/bg-3.jpg')",
+        smoke: "url('/assets/gif-smoke.gif')"
+      }
+    }
+  },
+  plugins: []
+}
